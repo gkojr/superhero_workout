a8a71536a7b80207f2c20e479576c63a31ab3377
gkojr <georgekowusujr@gmail.com>
2024-02-10 08:34:12 -0500

    morning edit

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,20 @@
+const heroNames = ['batman', 'spiderman', 'superman', 'wonderwoman', 'greenlantern']
+
 $(document).ready(function() {
+
+    function populateCards() {
+        const cardContainer = document.getElementById("cards-container"); 
+    
+        cardsContainer.innerHTML = "";
+    
+        heroNames.forEach((heroName, index) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+    
+            //const img = 
+        })
+    
+    }
 
     function getHeroData(name) { //gets the json info from the data
         $.getJSON('/hero/' + name, function(data) {
