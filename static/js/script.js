6b36221d03fd97e512b198e1e0e956da4af01a00
gkojr <georgekowusujr@gmail.com>
2024-02-09 23:33:30 -0500

    added stuff to script

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    function getHeroData(name) { //gets the json info from the data
+        $.getJSON('/hero/' + name, function(data) {
+            
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
