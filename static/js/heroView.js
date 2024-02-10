37091dde7963cc14b4679138d1925c1ca8c4cad7
gkojr <georgekowusujr@gmail.com>
2024-02-10 14:08:08 -0500

    set up things for frontend intergration

--- static/js/heroView.js
+++ static/js/heroView.js
@@ -2,40 +2,60 @@
 var heroId = window.location.pathname.split('/').pop();
 //alert(heroId);
 
+
 $(document).ready(function() {
 
     //get hero name and get the image for the hero
     $.getJSON('/heroID/' + heroId, function(data) {
         //get name and use for title
-        const heroName = data.name;
-        alert(heroName);
-        $('.hero-title').text(heroName);
+        //const heroName = data.name;
+        //alert(heroName);
+        const heroName = data.name; 
+        //$('.hero-description').text(heroName);
 
         var heroImageUrl = data.imageUrl;
         // Update the src attribute of the "hero-image" img element
         $('.hero-image').attr('src', heroImageUrl);
-    })
 
-    //get the description for the hero
-    $.getJSON('/description/' + heroName, function(data) {
-        $('.hero-description').text("hello");
-    })
-
-    function showWorkout(heroID) {
-        //window.location.href = `workout.html?heroID=${heroId}`;
-        // Get reference to the div
-        var div = document.getElementById('workout');
-        // Display text in the div
-        $.getJSON('/workout/' + heroName, function(data) {
-            //$('.hero-description').text(data);
-            const workout = data; 
+        $.getJSON('/workout/' + heroName + '/' + 12 + '/' + 72 + '/' + 180 + '/' + false, function(data) {
+            $('.hero-description').text(data);
         })
-        div.textContent = workout;
-        div.style.display = 'block'; // Show the div
-    }
 
-    function showDietPlan(heroID) {
-        //window.location.href = `diet.html?heroID=${heroId}`;
-    }
+        function showWorkout(heroID) {
+            //window.location.href = `workout.html?heroID=${heroId}`;
+            // Get reference to the div
+            var div = document.getElementById('workout');
+            // Display text in the div
+            $.getJSON('/workout/' + heroName + '/' + 12 + '/' + 72 + '/' + 180 + '/' + false, function(data) {
+                //$('.hero-description').text(data);
+                const workout = data; 
+                div.textContent = workout;
+                div.style.display = 'block'; // Show the div
+            })
+            
+        }
+    
+        function showDietPlan(heroID) {
+            //window.location.href = `diet.html?heroID=${heroId}`;
+            var div = document.getElementById('diet');
+            // Display text in the div
+            $.getJSON('/diet/' + heroName + '/' + 12 + '/' + 72 + '/' + 180 + '/' + false, function(data) {
+                //$('.hero-description').text(data);
+                const workout = data; 
+                div.textContent = workout;
+                div.style.display = 'block'; // Show the div
+            })
+        }
+
+
+
+        //get the description for the hero
+        /*$.getJSON('/description/' + heroName, function(data) {
+            //$('.hero-description').text("hello");
+            alert("hello");
+        })*/
+
+    })
+
 
 });
